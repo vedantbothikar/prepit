f76806dd43f8c4054e6fc56020b6ed830dc5990a
Vedant Bothikar <vbothikar2001@gmail.com>
2022-08-31 14:06:46 +0530

    pattern 2

--- DSA - Practice/practice codes/patterns/pattern2/code.cpp
+++ DSA - Practice/practice codes/patterns/pattern2/code.cpp
@@ -3,6 +3,22 @@ using namespace std;
 
 int main () {
 
+    int n;
+    cin>>n;
+
+    int row = 0;
+
+    while(row<n){
+        int col=0;
+        while (col<n)
+        {
+            cout<<col+1<<" ";
+            col += 1;
+        }
+        row += 1;
+        cout<<endl;
+    }
+
 
     return 0;
 }
--- DSA - Practice/practice codes/patterns/pattern2/notes.md
+++ DSA - Practice/practice codes/patterns/pattern2/notes.md
@@ -1,1 +1,11 @@
 #Notes
+
+display pattern:
+
+1 2 3 4
+1 2 3 4
+1 2 3 4
+1 2 3 4
+
+Observation:
+inside every row, the column's value is `col+1`
