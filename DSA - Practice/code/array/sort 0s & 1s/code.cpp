f5954aba7f39ef103521191c21bf69e0e2fc005f
Vedant Bothikar <vbothikar2001@gmail.com>
2022-09-06 21:54:42 +0530

    segregate 0s & 1s in array

---
+++ DSA - Practice/code/array/sort 0s & 1s/code.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printIntArray(int arr[], int size)
+{
+    cout << "Printing Array" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+}
+
+int countZeroes(int arr[], int n)
+{
+
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == 0)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+void segregate(int arr[], int n, int count)
+{
+
+    for (int i = 0; i < count; i++)
+    {
+        arr[i] = 0;
+    }
+
+    for (int i = count; i < n; i++)
+    {
+        arr[i] = 1;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[100];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int count = countZeroes(arr, n);
+    segregate(arr, n, count);
+    printIntArray(arr, n);
+
+    return 0;
+}
---
+++ DSA - Practice/code/common/frequent/frequent-functions.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printIntArray(int arr[], int size)
+{
+    cout << "Printing Array" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+
+    return 0;
+}
