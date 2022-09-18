df656721841fd0233fe37c3143f1bc7657584b8f
Vedant Bothikar <vbothikar2001@gmail.com>
2022-09-19 01:23:01 +0530

    19th sep 1:22 am update

---
+++ OOPS/code/basic-sample-class-object/code.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include "myclasses.cpp"
+using namespace std;
+
+int main()
+{
+
+    Car c1;
+    Pen p1;
+
+    cout << "size of car object is: " << sizeof(c1) << endl;
+    cout << "size of pen object is: " << sizeof(p1) << endl;
+
+    return 0;
+}
