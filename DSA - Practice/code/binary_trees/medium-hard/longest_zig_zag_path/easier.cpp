533fea92e6b372815df0ea921564de7b989800cc
vedantbothikar <vbothikar2001@gmail.com>
2024-08-14 15:48:44 -0400

    easier sol

--- DSA - Practice/code/binary_trees/medium-hard/longest_zig_zag_path/easier.cpp
+++ DSA - Practice/code/binary_trees/medium-hard/longest_zig_zag_path/easier.cpp
@@ -1,14 +1,10 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+/*
+
+https://www.youtube.com/watch?v=bAO794rh_4M&t=11s
+
+Watch video
+
+*/
 class Solution {
 public:
 
