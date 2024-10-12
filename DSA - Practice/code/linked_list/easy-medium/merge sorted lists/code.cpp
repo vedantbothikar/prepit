59d084dd8e2d389f0bb9e0561e258c3a317623be
vedantbothikar <vbothikar2001@gmail.com>
2024-10-12 14:17:30 -0400

    merge sorted lists

--- DSA - Practice/code/linked_list/easy-medium/merge sorted lists/code.cpp
+++ DSA - Practice/code/linked_list/easy-medium/merge sorted lists/code.cpp
@@ -1,11 +1,3 @@
-
-
-/*
-
-PROBLEM: https://leetcode.com/problems/merge-two-sorted-lists/
-
-*/
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -18,62 +10,67 @@ PROBLEM: https://leetcode.com/problems/merge-two-sorted-lists/
  */
 
 /*
-COMMENTS:
-- Check if we can do it in constance space?
 
-APPROACH:
-
-traverse both lists together
-We will take any one list of them, 
-access the elements one by one,
-- compare it with element of the other list,
-- choose the smallest one amongst them and attach it to the result list
-- increment the pointer of the list of which element has been chosen
-
-- Now when we reach the end of any one list, then break the loop
-- Now loop in another list and attach all element of that list to the result and return it
-
+- https://leetcode.com/problems/merge-two-sorted-lists/
 
 */
 
-
 class Solution {
+
+    ListNode* solve(ListNode* head1, ListNode* head2) {
+
+        ListNode* start = new ListNode(0);
+        ListNode* current = start;  // use current to traverse the list
+
+        while(head1 != NULL and head2 != NULL) {
+
+            if(head1->val < head2->val) {
+
+                current->next = new ListNode(head1->val);
+                head1 = head1->next;
+            }
+            else if(head2->val < head1->val) {
+                
+                current->next = new ListNode(head2->val);
+                head2 = head2->next;
+            }
+            else {  // this means both values are equal
+
+                current->next = new ListNode(head1->val);
+                head1 = head1->next;
+
+                current = current->next;
+
+                current->next = new ListNode(head2->val);
+                head2 = head2->next;
+            }
+
+            current = current->next;
+        }
+
+        // merge all remaining nodes
+        while(head1 != NULL) {
+
+            current->next = new ListNode(head1->val);
+            
+            head1 = head1->next;
+            current = current->next;
+        }
+
+        while(head2 != NULL) {
+
+            current->next = new ListNode(head2->val);
+            
+            head2 = head2->next;
+            current = current->next;
+        }
+
+        return start->next;
+    }
+
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-
-        ListNode finalRes(0);
-        ListNode *res = &finalRes;
-
-        while(list1 != nullptr && list2 != nullptr) {
-
-            if(list1->val < list2->val) {
-                
-                res->next = list1;
-                list1 = list1->next;
-            }
-            else {
-
-                res->next = list2;
-                list2 = list2->next;
-            }
-
-            res = res->next;
-        }
-
-        while(list1 != nullptr) {
-            
-            res->next = list1;
-            res = res->next;
-            list1 = list1->next;
-        }
-
-        while(list2 != nullptr) {
-
-            res->next = list2;
-            res = res->next;
-            list2 = list2->next;
-        }
-
-        return finalRes.next;
+        
+        return solve(list1, list2);
     }
 };
