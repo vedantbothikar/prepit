4e7b17d964c797f7b3b211f0d5c0d5a6c4d157b9
vedantbothikar <vbothikar2001@gmail.com>
2024-10-25 18:38:13 -0400

    kth largest in a strem heaps

--- DSA - Practice/code/heaps/medium/is binary tree a heap/code.cpp
+++ DSA - Practice/code/heaps/medium/is binary tree a heap/code.cpp
@@ -5,6 +5,19 @@
 
 */
 
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
 
 class Solution {
     
