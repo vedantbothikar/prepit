b7e3adc11c7851fd9f181d3b26ad45e166ebe3b8
vedantbothikar <vbothikar2001@gmail.com>
2024-11-17 18:16:32 -0500

    min stack

---
+++ DSA - Practice/code/stacks/medium/155. Min Stack/code.cpp
@@ -0,0 +1,55 @@
+/*
+
+Maintain another stack called minStack that holds the minimum element at the current position of the curernt element element at the original stack.
+
+Watch: https://youtu.be/qkLl7nAwDPo
+
+*/
+class MinStack {
+public:
+
+    stack<int> s;
+    stack<int> minStack;
+
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+
+        s.push(val);
+        
+        if(minStack.empty()) {
+            minStack.push(val);
+        }
+        else {
+            int num = min(minStack.top(), val);
+            minStack.push(num);
+        }
+    }
+    
+    void pop() {
+        
+        s.pop();
+        minStack.pop();
+    }
+    
+    int top() {
+        
+        return s.top();
+    }
+    
+    int getMin() {
+        
+        return minStack.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
